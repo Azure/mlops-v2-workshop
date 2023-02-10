6af21d3459e3fc7a1b6359e9826720a6411603ff
Elena Neroslavskaya <lenisha@gmail.com>
2023-02-10 15:33:06 -0500

    simplify gh actions script

---
+++ ci-cd/ghactions/create-endpoint.sh
@@ -0,0 +1,11 @@
+yaml_path=$1
+name=$2 
+
+
+endpoint=$(az ml online-endpoint list  --query "[].{Name:name}"  --output table | grep -q '$name' )
+if [[ -z "$endpoint" ]]
+then
+  az ml online-endpoint create --name $name -f $1 
+else
+  echo "Endpoint $name exists"
+fi       
